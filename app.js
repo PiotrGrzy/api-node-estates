20fbd8e554d956a52b921c869b14402566875ac6
PiotrGrzy <48014334+PiotrGrzy@users.noreply.github.com>
2019-12-10 20:13:34 +0100

    add "/" route handler

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const estateRouter = require('./routes/estateRoutes');
-const usersRouter = require('./routes/usersRoutes');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const estateRouter = require("./routes/estateRoutes");
+const usersRouter = require("./routes/usersRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
 // MIDDLEWARE
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -19,11 +19,17 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-app.use('/api/v1/estates', estateRouter);
-app.use('/api/v1/users', usersRouter);
+app.use("/api/v1/estates", estateRouter);
+app.use("/api/v1/users", usersRouter);
+app.use("/", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    data: "Welcome in estates api"
+  })
+);
 
 // handling unhandled routes
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on the server!`));
 });
 
