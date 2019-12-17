330db50b67925beb76761466d635f3611305942e
unknown <piotr.grzymowicz1@gmail.com>
2019-12-17 16:42:51 +0100

    remove file upload

--- routes/estateRoutes.js
+++ routes/estateRoutes.js
@@ -2,40 +2,40 @@ const express = require("express");
 const estateController = require("../controllers/estateController");
 const authController = require("../controllers/authController");
 const router = express.Router();
-const multer = require("multer");
+// const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    // cb = callback
-    cb(null, "./uploads/");
-  },
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: function(req, file, cb) {
+//     // cb = callback
+//     cb(null, "./uploads");
+//   },
+//   filename: function(req, file, cb) {
+//     cb(null, Date.now() + file.originalname);
+//   }
+// });
 
-const fileFilter = (req, file, cb) => {
-  // not working atm
-  if (file.mimetype === "image/png" || file.mimetype === "image/jpg") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+// const fileFilter = (req, file, cb) => {
+//   // not working atm
+//   if (file.mimetype === "image/png" || file.mimetype === "image/jpg") {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
 
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 4 //max 4MB
-  }
-});
+// const upload = multer({
+//   storage: storage,
+//   limits: {
+//     fileSize: 1024 * 1024 * 4 //max 4MB
+//   }
+// });
 
 router
   .route("/")
   .get(estateController.getAllEstates)
   .post(
     authController.protect,
-    upload.single("photo"),
+    // upload.single("photo"),
     estateController.createEstate
   );
 
@@ -44,7 +44,7 @@ router
   .get(estateController.getEstate)
   .patch(
     authController.protect,
-    upload.single("photo"),
+    // upload.single("photo"),
     estateController.updateEstate
   )
   .delete(authController.protect, estateController.deleteEstate);
